453c2f6ea782813e2f084b5eaffc260f5ee0c4ea
Nura8815 <118092206+Nura8815@users.noreply.github.com>
2023-03-23 13:32:26 +0300

    Update DesignsControll.swift

--- Sources/App/Controllers/DesignsControll.swift
+++ Sources/App/Controllers/DesignsControll.swift
@@ -5,16 +5,59 @@
 //  Created by nura on 28/08/1444 AH.
 //
 
-import SwiftUI
+import Fluent
+import Vapor
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct DesignsControllr: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let designs = routes.grouped("designs")
+        designs.get(use: index)
+        designs.get(":designsID",use: getid)
+        designs.post(use: create)
+        designs.put(":designsID",use: update)
+        designs.delete(":designsID",use: delete(req:))
     }
-}
+    
 
-struct SwiftUIView_Previews: PreviewProvider {
-    static var previews: some View {
-        SwiftUIView()
+    func index(req: Request) async throws -> [Designs] {
+        try await Designs.query(on: req.db).all()
+    }
+    
+    
+
+    func create(req: Request) async throws -> Designs {
+        let designs = try req.content.decode(Designs.self)
+        try await designs.save(on: req.db)
+        return designs
+    }
+
+    
+    func delete(req: Request) async throws -> HTTPStatus {
+    guard let designs = try await Designs.find(req.parameters.get("designsID"), on: req.db) else {
+    throw Abort(.notFound)
+    }
+    try await designs.delete(on: req.db)
+    return .noContent
+    }
+    
+    // update using specific id
+    func update(req: Request) async throws -> HTTPStatus {
+    let designes = try req.content.decode(Designs.self)
+    guard let des = try await Designs.find(designes.id,on: req.db) else {
+    throw Abort(.notFound)
+    }
+    des.name = designes.name
+    des.info = designes.info
+    des.t_collection = designes.t_collection
+    des.photo = designes.photo
+    try await des.save(on:req.db)
+    return .ok
+
+    }
+    func getid(req: Request) async throws -> Designs {
+    guard let designs = try await Designs.find(req.parameters.get("designsID"), on: req.db) else {
+    throw Abort(.notFound)
+    }
+    return designs
     }
 }
